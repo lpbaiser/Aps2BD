5def05c726ee62af92585725d8e94e76031a62a8
leonardo <leonardo@baiser>
2015-11-26 14:44:08 -0200

    Bloqueio Bifasico

---
+++ src/Arquivo/Arquivo.java
@@ -0,0 +1,22 @@
+package Arquivo;
+
+import Model.Trasacao;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Document: Arquivo
+ * @author : Leonardo Baiser <lpbaiser@gmail.com>
+ * @since : Nov 26, 2015, 2:21:12 PM
+ */
+public class Arquivo {
+    
+    public List<Trasacao> readArquivo(String path){
+        List<Trasacao> transacoes = new ArrayList<>();
+        
+        
+        
+        return transacoes;
+    }
+
+}
